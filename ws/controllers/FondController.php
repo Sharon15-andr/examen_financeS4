5c3cdd3d7c1faadcabaf80aa83039dff85c51cf1
Sharon15-andr <sharonnnandriia@gmail.com>
2025-07-08 12:17:31 +0300

    Update FondController.php

--- ws/controllers/FondController.php
+++ ws/controllers/FondController.php
@@ -1,1 +1,11 @@
+<?php
+require_once __DIR__ . '/../models/Fond.php';
+
+class FondController {
+    public static function getAll()     { Flight::json(Fond::getAll()); }
+    public static function create() {
+        $id = Fond::create(Flight::request()->data);
+        Flight::json(['message' => 'Fond ajouté', 'id' => $id]);
+    }
+}
 
