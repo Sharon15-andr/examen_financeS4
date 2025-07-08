0aac5419d0d32ddbb8a7239330561e70b7403a4e
leonfrankyNC <leonfrankyandriantsoa@gmail.com>
2025-07-08 12:21:05 +0300

    Update TypePretController.php

--- ws/controllers/TypePretController.php
+++ ws/controllers/TypePretController.php
@@ -1,1 +1,11 @@
+<?php
+require_once __DIR__ . '/../models/TypePret.php';
+
+class TypePretController {
+    public static function getAll()     { Flight::json(TypePret::getAll()); }
+    public static function create() {
+        $id = TypePret::create(Flight::request()->data);
+        Flight::json(['message' => 'Type de prêt ajouté', 'id' => $id]);
+    }
+}
 
